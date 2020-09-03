465c382c7a89f4dc25da96c99f338fe83762309a
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-09-03 14:43:41 +0200

    Update QRCamera_new.py

--- QRCamera_new.py
+++ QRCamera_new.py
@@ -7,15 +7,15 @@ from pyzbar.pyzbar import decode, ZBarSymbol
 import cv2
 import pyautogui
 import numpy as np
+import threading
 from PIL import Image, ImageTk, ImageDraw
 import os
-
+ 
 class App:
     def __init__(self,font_video=0):
         self.active_camera = False
         self.info = []
-        self.content = ""
-        self.appName = 'QR/BAR Code Reader'
+        self.appName = 'QR Code Reader'
         self.ventana = Tk()
         self.ventana.title(self.appName)
         self.ventana['bg']='black'
@@ -24,11 +24,11 @@ class App:
                          fg='white').pack(side=TOP,fill=BOTH)
         self.btnSave = Button(self.ventana,text="GUARDAR INFO",bg='light blue',command=self.guardar)
         self.btnSave.pack(side=BOTTOM)
-        
+ 
         self.display=scrolledtext.ScrolledText(self.ventana,width=86,background='snow3'
                                         ,height=4,padx=10, pady=10,font=('Arial', 10))
         self.display.pack(side=BOTTOM)
-        
+ 
         self.canvas=Canvas(self.ventana,bg='black',width=640,height=0)
         self.canvas.pack()
         self.btnLoad = Button(self.ventana,text="CARGAR ARCHIVO",width=29,bg='goldenrod2',
@@ -40,58 +40,53 @@ class App:
         self.btnScreen = Button(self.ventana,text="DETECTAR EN PANTALLA",width=29,bg='goldenrod2',
                                 activebackground='red',command=self.screen_shot)
         self.btnScreen.pack(side=RIGHT)
-
+ 
         self.ventana.mainloop()
-
+ 
     def guardar(self):
-        if self.content != "":
+        if len(self.display.get('1.0',END))>1:
             documento=open('QR_info.txt',"w",encoding="utf-8")
             linea=""
-            for c in str(self.content):
+            for c in str(self.display.get('1.0',END)):
                 linea=linea+c
             documento.write(linea)
             documento.close()
             messagebox.showinfo("GUARDADO","INFORMACIÓN GUARDADA EN \'QR_info.txt\'")
-        
+ 
     def abrir(self):
-        self.content = ""
         ruta = filedialog.askopenfilename(initialdir="/",title="SELECCIONAR ARCHIVO",
                     filetypes =(("png files","*.png") ,("jpg files","*.jpg")))
         if ruta != "":
             archivo = cv2.imread(ruta)
-            self.info = decode(archivo)
+            self.info = decode(archivo,symbols=[ZBarSymbol.QRCODE])
             if self.info != []:
                 self.display.delete('1.0',END)
-                self.display.insert(END,self.info[0][0])
-                self.content = self.info[0][0]
+                for i in self.info:
+                    print("n")
+                    self.display.insert(END,(str(i[0])+'\n'))
             else:
                 messagebox.showwarning("ERROR","NO SE DETECTÓ CÓDIGO")
-                self.display.delete('1.0',END)
-        
+ 
     def screen_shot(self):
-        self.content = ""
         pyautogui.screenshot("QRsearch_screenshoot.jpg")
         archivo = cv2.imread("QRsearch_screenshoot.jpg")
         self.info = decode(archivo)
         if self.info != []:
             self.display.delete('1.0',END)
             self.display.insert(END,self.info[0][0])
-            self.content = self.info[0][0]
         else:
             messagebox.showwarning("QR NO ENCONTRADO","NO SE DETECTÓ CÓDIGO")
-            self.display.delete('1.0',END)
         os.remove("QRsearch_screenshoot.jpg")
-        
+ 
     def visor(self):
         ret, frame=self.get_frame()
         if ret:
             self.photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
             self.canvas.create_image(0,0,image=self.photo,anchor=NW)
             self.ventana.after(15,self.visor)
-                    
+ 
     def active_cam(self):
         if self.active_camera == False:
-            self.content = ""
             self.active_camera = True
             self.VideoCaptura()
             self.visor()
@@ -101,7 +96,7 @@ class App:
             self.vid.release()
             self.canvas.delete('all')
             self.canvas.configure(height=0)
-            
+ 
     def capta(self,frm):
         self.info = decode(frm)
         cv2.putText(frm, "Muestre el codigo delante de la camara para su lectura", (84, 37), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
@@ -109,8 +104,7 @@ class App:
             self.display.delete('1.0',END)
             self.display.insert(END,self.info[0][0])
             self.draw_rectangle(frm)
-            self.content = self.info[0][0]
-
+ 
     def get_frame(self):
         if self.vid.isOpened():
             verif,frame=self.vid.read()
@@ -122,14 +116,13 @@ class App:
                 messagebox.showwarning("CAMARA NO DISPONIBLE","""La cámara está siendo utilizada por otra aplicación.
                 Cierrela e intentelo de nuevo.""")
                 self.active_cam()
-                self.display.delete('1.0',END)
                 return(verif,None)
         else:
             verif=False
             return(verif,None)
-        
+ 
     def draw_rectangle(self,frm):
-        #codes = decode(frm,symbols=[ZBarSymbol.QRCODE])
+       # codes = decode(frm,symbols=[ZBarSymbol.QRCODE])
         codes = decode(frm)
         for code in codes:
             data = code.data.decode('ascii')
@@ -138,7 +131,7 @@ class App:
             cv2.rectangle(frm, (x,y),(x+w, y+h),(255, 0, 0), 6)
             cv2.putText(frm, "QR Code", (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 50, 255), 2)
             #cv2.rectangle(frm, code.polygon[0], code.polygon[1],(0, 255, 0), 4)###############################################################
-        
+ 
     def VideoCaptura(self):
         self.vid = cv2.VideoCapture(self.font_video)
         if self.vid.isOpened():
@@ -149,11 +142,11 @@ class App:
             messagebox.showwarning("CAMARA NO DISPONIBLE","El dispositivo no está activado o disponible")
             self.display.delete('1.0',END)
             self.active_camera = False
-
+ 
     def __del__(self):
         if self.active_camera == True:
             self.vid.release()
-
+ 
 if __name__=="__main__":
     App()
 
