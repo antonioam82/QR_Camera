deeafcfad2d5427081a29f5c766a7828d13a3499
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-12-27 15:34:58 +0100

    Update QR_Camera3.py

--- QR_Camera3.py
+++ QR_Camera3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from tkinter import *
+import tkinter as tk
 import tkinter.scrolledtext as scrolledtext
 from tkinter import messagebox, filedialog
 from pyzbar.pyzbar import decode, ZBarSymbol
@@ -18,43 +18,43 @@ class App:
         self.info = []
         self.codelist = []
         self.appName = 'QR Code Reader'
-        self.ventana = Tk()
+        self.ventana = tk.Tk()
         self.ventana.title(self.appName)
-        self.ventana.resizable(height=FALSE,width=FALSE)##
+        self.ventana.resizable(height=tk.FALSE,width=tk.FALSE)##
         self.ventana['bg']='black'
-        self.currentDir = StringVar()
+        self.currentDir = tk.StringVar()
         self.currentDir.set(os.getcwd())
         self.font_video=font_video
-        Entry(self.ventana,textvariable=self.currentDir,width=107).pack(side=TOP)
-        Label(self.ventana,text=self.appName,font=15,bg='blue',
-                         fg='white').pack(side=TOP,fill=BOTH)
-        bt_canvas = Canvas(self.ventana)
-        bt_canvas.pack(side=BOTTOM)
-        Button(bt_canvas,text="GUARDAR",width=10,bg='light blue',command=self.guardar).pack(side=LEFT)
-        Button(bt_canvas,text="COPIAR",width=10,bg='light blue',command=self.copy_info).pack(side=LEFT)
+        tk.Entry(self.ventana,textvariable=self.currentDir,width=107).pack(side=tk.TOP)
+        tk.Label(self.ventana,text=self.appName,font=15,bg='blue',
+                         fg='white').pack(side=tk.TOP,fill=tk.BOTH)
+        bt_canvas = tk.Canvas(self.ventana)
+        bt_canvas.pack(side=tk.BOTTOM)
+        tk.Button(bt_canvas,text="GUARDAR",width=10,bg='light blue',command=self.guardar).pack(side=tk.LEFT)
+        tk.Button(bt_canvas,text="COPIAR",width=10,bg='light blue',command=self.copy_info).pack(side=tk.LEFT)
  
         self.display=scrolledtext.ScrolledText(self.ventana,width=86,background='snow3'
                                         ,height=4,padx=10, pady=10,font=('Arial', 10))
-        self.display.pack(side=BOTTOM)
+        self.display.pack(side=tk.BOTTOM)
  
-        self.canvas=Canvas(self.ventana,bg='black',width=640,height=0)
+        self.canvas=tk.Canvas(self.ventana,bg='black',width=640,height=0)
         self.canvas.pack()
-        Button(self.ventana,text="CARGAR ARCHIVO",width=29,bg='goldenrod2',activebackground='red',command=self.abrir).pack(side=LEFT)
-        self.btnCamera=Button(self.ventana,text="INICIAR LECTURA POR CAMARA",width=30,bg='goldenrod2',
+        tk.Button(self.ventana,text="CARGAR ARCHIVO",width=29,bg='goldenrod2',activebackground='red',command=self.abrir).pack(side=tk.LEFT)
+        self.btnCamera=tk.Button(self.ventana,text="INICIAR LECTURA POR CAMARA",width=30,bg='goldenrod2',
                                 activebackground='red',command=self.active_cam)
-        self.btnCamera.pack(side=LEFT)
-        Button(self.ventana,text="DETECTAR EN PANTALLA",width=29,bg='goldenrod2',activebackground='red',command=self.screen_shot).pack(side=RIGHT)
+        self.btnCamera.pack(side=tk.LEFT)
+        tk.Button(self.ventana,text="DETECTAR EN PANTALLA",width=29,bg='goldenrod2',activebackground='red',command=self.screen_shot).pack(side=tk.RIGHT)
  
         self.ventana.mainloop()
  
     def guardar(self):
-        if len(self.display.get('1.0',END))>1:
+        if len(self.display.get('1.0',tk.END))>1:
             documento = filedialog.asksaveasfilename(initialdir="/",
                         title="Guardar en",defaultextension='.txt')
             if documento != "":
                 archivo_guardar = open(documento,"w",encoding="utf-8")
                 linea=""
-                for c in str(self.display.get('1.0',END)):
+                for c in str(self.display.get('1.0',tk.END)):
                     linea=linea+c
                 archivo_guardar.write(linea)
                 archivo_guardar.close()
@@ -62,8 +62,8 @@ class App:
 
     def copy_info(self):
         try:
-            if len(self.display.get('1.0',END))>1:
-                pyperclip.copy(self.display.get('1.0',END))
+            if len(self.display.get('1.0',tk.END))>1:
+                pyperclip.copy(self.display.get('1.0',tk.END))
                 messagebox.showinfo("COPIADO","Copiado en el portapapeles.")
         except Exception as e:
             messagebox.showwarning("UNEXPECTED ERROR",str(e))
@@ -75,9 +75,9 @@ class App:
             archivo = cv2.imread(ruta)
             self.info = decode(archivo)
             if self.info != []:
-                self.display.delete('1.0',END)
+                self.display.delete('1.0',tk.END)
                 for i in self.info:
-                    self.display.insert(END,(i[0].decode('utf-8'))+'\n')
+                    self.display.insert(tk.END,(i[0].decode('utf-8'))+'\n')
             else:
                 messagebox.showwarning("ARCHIVO NO VÁLIDO","NO SE DETECTÓ CÓDIGO QR.")
  
@@ -86,9 +86,9 @@ class App:
         archivo = cv2.imread("QRsearch_screenshoot.jpg")
         self.info = decode(archivo)
         if self.info != []:
-            self.display.delete('1.0',END)
+            self.display.delete('1.0',tk.END)
             for i in self.info:
-                self.display.insert(END,(i[0].decode('utf-8'))+'\n')
+                self.display.insert(tk.END,(i[0].decode('utf-8'))+'\n')
         else:
             messagebox.showwarning("QR NO ENCONTRADO","NO SE DETECTÓ CÓDIGO")
         os.remove("QRsearch_screenshoot.jpg")
@@ -97,7 +97,7 @@ class App:
         ret, frame=self.get_frame()
         if ret:
             self.photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
-            self.canvas.create_image(0,0,image=self.photo,anchor=NW)
+            self.canvas.create_image(0,0,image=self.photo,anchor=tk.NW)
             self.ventana.after(15,self.visor)
  
     def active_cam(self):
@@ -117,18 +117,18 @@ class App:
         self.info = decode(frm)
         cv2.putText(frm, "Muestre el codigo delante de la camara para su lectura", (84, 37), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
         if self.info != []:
-            self.display.delete('1.0',END)
+            self.display.delete('1.0',tk.END)
             for code in self.info:
                 if code not in self.codelist:
                     content = code[0].decode('utf-8')
                     self.codelist.append(content)
-                    self.display.insert(END,content+'\n')
+                    self.display.insert(tk.END,content+'\n')
                 self.draw_rectangle(frm)
         else:
             if len(self.codelist)>0:
-                self.display.delete('1.0',END)
+                self.display.delete('1.0',tk.END)
                 for e in set(self.codelist):
-                    self.display.insert(END,e+'\n')
+                    self.display.insert(tk.END,e+'\n')
  
     def get_frame(self):
         if self.vid.isOpened():
@@ -163,7 +163,7 @@ class App:
             self.canvas.configure(width=self.width,height=self.height)
         else:
             messagebox.showwarning("CAMARA NO DISPONIBLE","El dispositivo está desactivado o no disponible")
-            self.display.delete('1.0',END)
+            self.display.delete('1.0',tk.END)
             self.active_camera = False
  
     def __del__(self):
